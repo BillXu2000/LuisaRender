ac4080a7e3852f298c8ccdedb7829fdeba41db81
bx2k_2080 <billxu2000@outlook.com>
2023-03-12 17:09:11 +0800

    test direct

--- src/integrators/vcm.cpp
+++ src/integrators/vcm.cpp
@@ -1,10 +1,9 @@
-//
-// Created by Mike on 2022/1/7.
-//
-
 #include <util/rng.h>
 #include <base/pipeline.h>
 #include <base/integrator.h>
+
+#include <util/progress_bar.h>
+#include <base/display.h>
 
 namespace luisa::render {
 
@@ -32,36 +31,131 @@ public:
     using ProgressiveIntegrator::Instance::Instance;
 
 protected:
+    void _render_one_camera(CommandBuffer &command_buffer, Camera::Instance *camera) noexcept override {
+        auto spp = camera->node()->spp();
+        auto resolution = camera->film()->node()->resolution();
+        auto image_file = camera->node()->file();
+
+        auto pixel_count = resolution.x * resolution.y;
+        sampler()->reset(command_buffer, resolution, pixel_count, spp);
+        command_buffer << compute::synchronize();
+
+        LUISA_INFO(
+            "Rendering to '{}' of resolution {}x{} at {}spp.",
+            image_file.string(),
+            resolution.x, resolution.y, spp);
+
+        using namespace luisa::compute;
+
+        Kernel2D render_kernel = [&](UInt frame_index, Float time, Float shutter_weight) noexcept {
+            set_block_size(16u, 16u, 1u);
+            auto pixel_id = dispatch_id().xy();
+            auto L = Li_vcm(camera, frame_index, pixel_id, time, shutter_weight);
+            camera->film()->accumulate(pixel_id, shutter_weight * L);
+        };
+
+        Clock clock_compile;
+        auto render = pipeline().device().compile(render_kernel);
+        auto integrator_shader_compilation_time = clock_compile.toc();
+        LUISA_INFO("Integrator shader compile in {} ms.", integrator_shader_compilation_time);
+        auto shutter_samples = camera->node()->shutter_samples();
+        command_buffer << synchronize();
+
+        LUISA_INFO("Rendering started.");
+        Clock clock;
+        ProgressBar progress;
+        progress.update(0.);
+        auto dispatch_count = 0u;
+        auto sample_id = 0u;
+        for (auto s : shutter_samples) {
+            pipeline().update(command_buffer, s.point.time);
+            for (auto i = 0u; i < s.spp; i++) {
+                command_buffer << render(sample_id++, s.point.time, s.point.weight)
+                                    .dispatch(resolution);
+                auto dispatches_per_commit =
+                    display()->should_close() ?
+                        node<ProgressiveIntegrator>()->display_interval() :
+                        32u;
+                if (++dispatch_count % dispatches_per_commit == 0u) [[unlikely]] {
+                    dispatch_count = 0u;
+                    auto p = sample_id / static_cast<double>(spp);
+                    if (display()->update(command_buffer, sample_id)) {
+                        progress.update(p);
+                    } else {
+                        command_buffer << [&progress, p] { progress.update(p); };
+                    }
+                }
+            }
+        }
+        command_buffer << synchronize();
+        progress.done();
+
+        auto render_time = clock.toc();
+        LUISA_INFO("Rendering finished in {} ms.", render_time);
+    }
+
     [[nodiscard]] Float3 Li(const Camera::Instance *camera, Expr<uint> frame_index,
                             Expr<uint2> pixel_id, Expr<float> time) const noexcept override {
+        return Li_vcm(camera, frame_index, pixel_id, time, 1.f);
+    }
+    [[nodiscard]] Float3 Li_vcm(const Camera::Instance *camera, Expr<uint> frame_index,
+                            Expr<uint2> pixel_id, Expr<float> time, Expr<float> shutter_weight) const noexcept {
         sampler()->start(pixel_id, frame_index);
         auto u_filter = sampler()->generate_pixel_2d();
         auto u_lens = camera->node()->requires_lens_sampling() ? sampler()->generate_2d() : make_float2(.5f);
         auto cs = camera->generate_ray(pixel_id, time, u_filter, u_lens);
-        auto swl = pipeline().spectrum()->sample(sampler()->generate_1d());
-        auto path_weight = cs.weight;
-        auto it = pipeline().geometry()->intersect(cs.ray);
-        auto ns = def(make_float3(0.f));
-        auto wo = -cs.ray->direction();
-        $if(it->valid()) {
-            if (node<VCM>()->shading()) {
-                $if(it->shape()->has_surface()) {
-                    pipeline().surfaces().dispatch(it->shape()->surface_tag(), [&](auto surface) noexcept {
-                        auto closure = surface->closure(it, swl, wo, 1.f, time);
-                        ns = closure->it()->shading().n();
-                    });
+        auto spectrum = pipeline().spectrum();
+        auto swl = spectrum->sample(spectrum->node()->is_fixed() ? 0.f : sampler()->generate_1d());
+        SampledSpectrum Li{swl.dimension(), 0.f};
+
+        auto ray = cs.ray;
+
+        $loop {
+
+            // trace
+            auto wo = -ray->direction();
+            auto it = pipeline().geometry()->intersect(ray);
+
+            // miss
+            $if(!it->valid()) {
+                if (pipeline().environment()) {
+                    auto eval = light_sampler()->evaluate_miss(ray->direction(), swl, time);
+                    Li += cs.weight * eval.L;
                 }
-                $else {
-                    ns = it->shading().n();
+                $break;
+            };
+
+            // hit light
+            if (!pipeline().lights().empty()) {
+                $if(it->shape()->has_light()) {
+                    auto eval = light_sampler()->evaluate_hit(*it, ray->origin(), swl, time);
+                    Li += cs.weight * eval.L;
                 };
-            } else {
-                ns = it->ng();
             }
-            if (node<VCM>()->remap()) {
-                ns = ns * .5f + .5f;
-            }
+
+            // compute direct lighting
+            $if(!it->shape()->has_surface()) { $break; };
+
+            auto light_sample = LightSampler::Sample::zero(swl.dimension());
+            auto occluded = def(false);
+
+            // sample one light
+            auto u_light_selection = sampler()->generate_1d();
+            auto u_light_surface = sampler()->generate_2d();
+            light_sample = light_sampler()->sample(
+                *it, u_light_selection, u_light_surface, swl, time);
+
+            // trace shadow ray
+            $if(light_sample.eval.pdf > 0.f &
+                light_sample.eval.L.any([](auto x) { return x > 0.f; })) {
+                occluded = pipeline().geometry()->intersect_any(light_sample.shadow_ray);
+            };
+            $if(light_sample.eval.pdf > 0.0f & !occluded) {
+                Li += cs.weight * light_sample.eval.L / light_sample.eval.pdf;
+            };
+            $break;
         };
-        return path_weight * ns;
+        return spectrum->srgb(swl, Li);
     }
 };
 
